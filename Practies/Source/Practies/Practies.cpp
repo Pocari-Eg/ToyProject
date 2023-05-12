b8d39e40970e89418dcd6478890baa44444ec941
Pocari-Eg <66935108+Pocari-Eg@users.noreply.github.com>
2023-05-12 15:32:50 +0900

    first Commit
    git ignore 적용 첫 커밋

---
+++ Practies/Source/Practies/Practies.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Practies.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Practies, "Practies" );
+
+DEFINE_LOG_CATEGORY(LogPracties)
+ 
